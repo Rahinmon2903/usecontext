751b6606490c3b7fd2df299e90c87d87c89ec9ef
Rahinmon2903 <rahin2903@gmail.com>
2025-11-04 19:26:32 +0530

    provider done

--- src/Components/CompD.jsx
+++ src/Components/CompD.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { myContext } from '../App';
 
 const CompD = () => {
+     const[name, setName]=useContext(myContext)
+    
+        const handleclick =()=>{
+            setName("sukin")
+        }
     return (
         <div>
+            <h1>{name}</h1>
+            <button onClick={handleclick}>change</button>
             
         </div>
     );
